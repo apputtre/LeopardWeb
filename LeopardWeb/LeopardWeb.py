687fa49e261ad17cd875a0b9a2280ccf253d2932
Alexander Puttre <apputtre@gmail.com>
2023-07-11 08:55:00 -0400

    integrated Quang's changes to User

--- LeopardWeb/LeopardWeb.py
+++ LeopardWeb/LeopardWeb.py
@@ -31,6 +31,29 @@ class User:
     def search_courses(self):
         print("Searching my courses...")
 
+    # Quang
+    def add_course(course):
+        # Check if the course is already enrolled
+        if course in self.courses:
+            print("You are already enrolled in this course.")
+        else:
+            # Check if the course has available slots
+            if len(course.students) < course.max_students:
+                self.courses.append(course)
+                course.add_student(self)
+                print("Added course:", course.name)
+            else:
+                print("Course is full. Unable to enroll.")
+
+    # Quang
+    def remove_course(self, course):
+        if course in self.courses:
+            self.courses.remove(course)
+            course.students.remove(self)
+            print("Removed course:", course.name)
+        else:
+            print("You are not enrolled in this course.")
+
 # Quang Vu & Alexander Puttre
 class Course(User):
     def __init__(self, id: str, title: str, department: str, time: str, days: str, semester: str, year: int, credits: int, max_students = 30):
