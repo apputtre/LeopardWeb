4e140b5192182612330a4cbf9b7c7a88669284ff
yasminahabchi <habchiy@wit.edu>
2023-07-15 14:55:44 -0400

    Instructor search courses function testing

--- LeopardWeb/test.py
+++ LeopardWeb/test.py
@@ -1,6 +1,8 @@
 import unittest
 import sqlite3
 from LeopardWeb import Admin
+from LeopardWeb import Instructor
+from LeopardWeb import Student
 from LeopardWeb import Course
 from Database import fetch_course
 
@@ -70,3 +72,39 @@ class TestAdminAddRemoveCourse(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+
+class TestInstructorSearchCourse(unittest.TestCase):
+
+    def setUp(self):
+        db = sqlite3.connect("assignment3.db")
+        dbcursor = db.cursor()
+
+        dbcursor.execute("DELETE FROM COURSES WHERE ID = 'id'")
+
+        db.commit()
+        db.close()
+
+        self.test_instructor = Instructor("first_name", "last_name", 0)
+        self.test_course = Course("id", "title", "department", "time", "days", "semester", 2023, 4, 25)
+
+    def tearDown(self):
+        pass
+
+    def test_search_course(self):
+        self.test_instructor.search_courses_menu(self.test_course)
+
+        db = sqlite3.connect("assignment3.db")
+        dbcursor = db.cursor()
+
+        search_result = fetch_course(dbcursor, "id")
+
+        assert len(search_result) == 1, f"Search result has incorrect length: {len(search_result)}"
+
+        course = Course.from_search_result(search_result[0], self.test_course.max_students)
+
+
+        db.commit()
+        db.close()
